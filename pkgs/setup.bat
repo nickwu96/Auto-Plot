18eaa037df7ed4ad819e6aa0d12b336825fe5ebf
nickwu96 <46734518+nickwu96@users.noreply.github.com>
2019-05-12 01:02:16 +0800

    add folder "pkgs"
    pkgs are required python packages for matplotlib(for plotting). If your computer can't get access to the Internet. You can download whl files in "pkgs", then run "setup.bat" to install the required packages automatically if you can't use the internet.

---
+++ pkgs/setup.bat
@@ -0,0 +1,9 @@
+pip install numpy-1.16.3+mkl-cp37-cp37m-win_amd64.whl
+pause
+pip install  pyparsing-2.4.0-py2.py3-none-any.whl
+pip install  pytz-2019.1-py2.py3-none-any.whl
+pip install  six-1.12.0-py2.py3-none-any.whl
+pip install  python_dateutil-2.8.0-py2.py3-none-any.whl
+pip install  cycler-0.10.0-py2.py3-none-any.whl
+pip install kiwisolver-1.1.0-cp37-cp37m-win_amd64.whl
+pip install  matplotlib-3.1.0rc2-cp37-cp37m-win_amd64.whl
